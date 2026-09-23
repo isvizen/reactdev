7f3df974a907cd4b5eb6f155e317373f716817ac
Moh Oktavi Aziz Nugraha <vizenuu@gmail.com>
2026-09-23 15:37:52 +0700

    Add "Choosing the State Structure"

---
+++ choosing-the-state-structure/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { letters } from "./data.js";
+import Letter from "./Letter.js";
+
+export default function MailClient() {
+  const [selectedIds, setSelectedIds] = useState(new Set());
+
+  const selectedCount = selectedIds.size;
+
+  function handleToggle(toggledId) {
+    const nextIds = new Set(selectedIds);
+    if (nextIds.has(toggledId)) {
+      nextIds.delete(toggledId);
+    } else {
+      nextIds.add(toggledId);
+    }
+    setSelectedIds(nextIds);
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={selectedIds.has(letter.id)}
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>You selected {selectedCount} letters</b>
+        </p>
+      </ul>
+    </>
+  );
+}
